7c85dcfebba8c3960a927f0fd05a230db39dc744
abdulrehmannjutt <abdulrehmanchahal15@gmail.com>
2025-01-21 02:03:27 +0500

    ui changes

--- client/src/layout/Header.jsx
+++ client/src/layout/Header.jsx
@@ -209,7 +209,7 @@ function Header() {
       )}
 
       <div
-        className={`fixed top-0 left-0 h-full w-64 bg-white shadow-lg transform transition-transform duration-300 ease-in-out z-[1000] ${
+        className={`fixed top-0 left-0 h-full w-[200px] bg-white shadow-lg transform transition-transform duration-300 ease-in-out z-[1000] ${
           isOpen ? "translate-x-0" : "-translate-x-full"
         }`}
       >
