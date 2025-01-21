59590c85ea372be3262094874ef3535aa489106a
abdulrehmannjutt <abdulrehmanchahal15@gmail.com>
2025-01-21 21:42:15 +0500

    constant changed

--- client/src/constants/constants.js
+++ client/src/constants/constants.js
@@ -1,2 +1,2 @@
-// export const BACKEND_BASE_URL = "https://shadewaveserver.vercel.app/";
-export const BACKEND_BASE_URL = "http://localhost:3000/";
+export const BACKEND_BASE_URL = "https://shadewaveserver.vercel.app/";
+// export const BACKEND_BASE_URL = "http://localhost:3000/";
