f9d4de1caa18860a13363f006b41cba459eaad27
abdulrehmannjutt <abdulrehmanchahal15@gmail.com>
2025-01-22 01:00:14 +0500

    change backendurl

--- client/src/constants/constants.js
+++ client/src/constants/constants.js
@@ -1,2 +1,3 @@
-export const BACKEND_BASE_URL = "https://shadewaveserver.vercel.app/";
+// export const BACKEND_BASE_URL = "https://shadewaveserver.vercel.app/";
+export const BACKEND_BASE_URL = "https://shadewave.up.railway.app/";
 // export const BACKEND_BASE_URL = "http://localhost:3000/";
